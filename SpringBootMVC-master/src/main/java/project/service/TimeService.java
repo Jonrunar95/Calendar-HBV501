44a99ba558bf6b645068e21a1a60c1face5436df
G10ckm4n <Pollinn@Gmail.com>
2018-10-12 23:34:44 +0000

    time
    ..

--- SpringBootMVC-master/src/main/java/project/service/TimeService.java
+++ SpringBootMVC-master/src/main/java/project/service/TimeService.java
@@ -20,7 +20,7 @@ public class TimeService{
         return startDate;
     }
 
-    public void setStartDate(int startDate) {
+    public void setStartDate(Date startDate) {
         this.startDate = startDate;
     }
 
@@ -28,7 +28,7 @@ public class TimeService{
         return end;
     }
 
-    public void setEnd(int end) {
+    public void setEnd(Date end) {
         this.end = end;
     }
 
