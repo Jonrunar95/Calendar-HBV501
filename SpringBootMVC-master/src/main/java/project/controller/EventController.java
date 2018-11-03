e22d724d78cb64556bd7369de8f29f0ebec5f73c
atlimp <amp17@hi.is>
2018-11-03 16:51:30 +0000

    bæta við comments í controller

--- SpringBootMVC-master/src/main/java/project/controller/EventController.java
+++ SpringBootMVC-master/src/main/java/project/controller/EventController.java
@@ -24,6 +24,9 @@ public class EventController {
         this.eventService = eventService;
     }
 
+    // Example: GET localhost:8080/event?StartDate={x}&endDate={y}
+    // x, y are dates in milliseconds
+    // Returns list of events with startDate between x and y
     @RequestMapping(path="/event", method=RequestMethod.GET)
     public List<Event> postEvent(@RequestParam String startDate, String endDate) {
         return eventService.findByDate(
@@ -32,16 +35,29 @@ public class EventController {
         );
     }
 
+    // Example: GET localhost:8080/event/{id}
+    // id is eventID
+    // Returns corresponding event
     @RequestMapping(path="/event/{id}", method=RequestMethod.GET)
     public Event findOneEvent(@PathVariable String id) {
         return eventService.findOne(Long.parseLong(id));
     }
 
+    // Example: POST localhost:8080/event
+    // RequestBody: { startDate, endDate, ownerID, title, description }
+    // startDate and endDate are dates in milliseconds
+    // ownerID is id of the creator of the event
+    // title and description are strings
+    // Returns event with new ID
     @RequestMapping(path="/event", method=RequestMethod.POST)
     public Event postEvent(@RequestBody Event event) {
         return eventService.save(event);
     }
 
+    // Example: POST localhost:8080/event/{id}
+    // RequestBody: [username_1, username_2, ... , username_k]
+    // id is eventID
+    // Returns event with updated UserList based on valid usernames given in request body
     @RequestMapping(path="/event/{id}", method=RequestMethod.POST)
     public Event updateUserList(@RequestBody List<String> usernames, @PathVariable String id) {
         return eventService.updateUserList(Long.parseLong(id), usernames);
--- SpringBootMVC-master/src/main/java/project/controller/UserController.java
+++ SpringBootMVC-master/src/main/java/project/controller/UserController.java
@@ -22,16 +22,25 @@ public class UserController {
         this.userService = userService;
     }
 
+    // Example: POST localhost:8080/user
+    // RequestBody: { name, username, hash }
+    // name, username and hash are strings.  username is unique.
+    // Returns user with new ID
     @RequestMapping(path="/user", method=RequestMethod.POST)
     public User registerUser(@RequestBody User user) {
         return userService.save(user);
     }
 
+    // Example: GET localhost:8080/user
+    // Returns list of all users
     @RequestMapping(path="/user", method=RequestMethod.GET)
     public List<User> listAllUsers() {
         return userService.findAll();
     }
 
+    // Example: GET localhost:8080/user/{id}
+    // id is userID
+    // Returns corresponding user, users eventList is in response.
     @RequestMapping(path="/user/{id}", method=RequestMethod.GET)
     public User findOneUser(@PathVariable String id) {
         return userService.findOne(Long.parseLong(id));
