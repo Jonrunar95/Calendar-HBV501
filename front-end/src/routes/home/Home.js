9ce1b8fc128f4d6ba323507ca91cfeff66802fb9
jonrunar <jonrunar@htgi-dw0291.rhi.hi.is>
2018-11-27 17:07:49 +0000

    home

--- front-end/src/routes/home/Home.js
+++ front-end/src/routes/home/Home.js
@@ -19,16 +19,14 @@ class Home extends Component {
   render() {
     
     return (
-        <div className='Buttons'>
+        <div className='Lion'>
           <div className = "homeLion"></div>
-          <div className='home-button'>
-            <Link className='button-link' to='/users'> Users </Link>
-          </div>
-          <div className='home-button'>
-            <Link className='button-link' to='/register'> Register</Link>
-          </div>
+          <div className='Vision'> This product is for people with dynamic scheduling needs and people who want to organise events with a group. A person whos schedule can change at any moment would profit from these changes being reflected in his schedule by the person responsible for the change. With dynamic event broadcasting any member of a meeting can notify others of a change.
+          The product is called `ÁætlunarStjórinn', it is a visual calendar scheduler that has secure connectivity and broadcasting of events as well as any changes to these events. This could save valuable time and resources. Unlike facebook which broadcasts events but does not have a visual scheduling service, or Android calendar app which has a visual scheduling interface but no interconnectivity or event creation.
+          Our product has Real time updates to visual calendars, which is unheard of in today's main- stream scheduling market.</div>
 
         </div>
+
     );
   }
 }
